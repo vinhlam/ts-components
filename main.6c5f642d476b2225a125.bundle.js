163fe9c1cd545100c551b72d3c6e60d4350649aa
Vinh Lam <vinh.lam@gmail.com>
2021-07-20 20:51:58 +0100

    initial commit

---
+++ main.6c5f642d476b2225a125.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1308:function(n,o,p){p(1309),p(1375),p(1408)},492:function(n,o,p){p(493),p(648),n.exports=p(1308)},558:function(n,o){}},[[492,1,2]]]);
---
+++ precache-manifest.1fc09afa4f7695d4e7b2d5fd152f9256.js
@@ -0,0 +1,32 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "15fccd117bbeb82a2a4ab6adc61f9293.png"
+  },
+  {
+    "url": "1dd5e74c1dc328f093dd01e1a67af6e0.png"
+  },
+  {
+    "url": "62f15370f5a94fcaee9a291e51e4d2d0.png"
+  },
+  {
+    "revision": "1d2953ff6e4b945aee2af9e9be6881b2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.57beeedf6a64b88b29e4.bundle.js"
+  },
+  {
+    "url": "runtime~main.57beeedf6a64b88b29e4.bundle.js"
+  },
+  {
+    "revision": "421e6487b1e6fe3d2fc0",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "revision": "0008d3493ede862efb3bc228c045d86c",
+    "url": "static/media/article-error.0008d349.png"
+  },
+  {
+    "url": "vendors~main.57beeedf6a64b88b29e4.bundle.js"
+  }
+]);
